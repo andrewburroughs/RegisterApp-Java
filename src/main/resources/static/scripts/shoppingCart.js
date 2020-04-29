54751ee9c9d006bcae65c430e95d6aaccf88566c
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-04-29 16:36:10 -0500

    Working

--- src/main/resources/static/scripts/shoppingCart.js
+++ src/main/resources/static/scripts/shoppingCart.js
@@ -8,6 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
     if(getContinueShoppingButtonElement() != null) {
         getContinueShoppingButtonElement().addEventListener("click", continueShopping);
     }
+    var list = document.getElementsByClassName("productPriceDisplay");
+	for(let i = 0; i<list.length; i++){
+		var x = list[i].innerHTML;
+		var len = x.length;
+		var pos = x.indexOf(".");
+		if(pos == -1){
+			x += ".";
+			pos = x.indexOf(".");
+		}
+		var diff = len - pos;
+		var newText = "$" + x;
+		for(let j = diff; j<3; j++){
+			newText += "0"
+		}
+		list[i].innerHTML = newText;
+	}
     calculateTotal();
 });
 
