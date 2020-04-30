23b836f9c585b5d35b0e595f847c9563574e108d
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-04-29 22:04:13 -0500

    Fixed Nonsense

--- src/main/resources/static/scripts/shoppingCart.js
+++ src/main/resources/static/scripts/shoppingCart.js
@@ -132,3 +132,22 @@ function updateQuantity() {
 function getTotalDisplayElement(){
     return document.getElementById("totalDisplay");
 }
+
+function findClickedListItemElement(clickedTarget) {
+	if (clickedTarget.tagName.toLowerCase() === "li") {
+		return clickedTarget;
+	} else {
+		let ancestorIsListItem = false;
+		let ancestorElement = clickedTarget.parentElement;
+
+		while (!ancestorIsListItem && (ancestorElement != null)) {
+			ancestorIsListItem = (ancestorElement.tagName.toLowerCase() === "li");
+
+			if (!ancestorIsListItem) {
+				ancestorElement = ancestorElement.parentElement;
+			}
+		}
+
+		return (ancestorIsListItem ? ancestorElement : null);
+	}
+}
