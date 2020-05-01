b0a643710e532b746be9a95729fb52ab41f0b050
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-05-01 17:18:00 -0500

    Working on Transaction Total

--- src/main/java/edu/uark/registerapp/commands/transactions/TransactionCommitCommand.java
+++ src/main/java/edu/uark/registerapp/commands/transactions/TransactionCommitCommand.java
@@ -15,6 +15,7 @@ import edu.uark.registerapp.models.entities.TransactionEntryEntity;
 import edu.uark.registerapp.models.repositories.ProductRepository;
 import edu.uark.registerapp.models.repositories.TransactionEntryRepository;
 import edu.uark.registerapp.models.repositories.TransactionRepository;
+import edu.uark.registerapp.models.entities.TransactionEntry;
 
 @Service
 public class TransactionCommitCommand implements VoidCommandInterface{
