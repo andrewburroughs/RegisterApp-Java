eddbdb034dabc83ac8fd2bd9747cc13666f88d14
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-05-12 23:08:26 -0500

    Sales Report Route Controller

---
+++ src/main/java/edu/uark/registerapp/controllers/SalesReportRouteController.java
@@ -0,0 +1,37 @@
+package edu.uark.registerapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
+
+@Controller
+@RequestMapping(value = "/salesReport")
+public class SalesReportRouteController {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView showSalesReport(){
+        ModelAndView modelAndView = 
+            new ModelAndView(ViewNames.SALES_REPORT.getViewName());
+        try {
+            modelAndView.addObject(
+                ViewModelNames.PRODUCTS.getValue(),
+                this.productsQuery.execute());
+        } catch (final Exception e) {
+            modelAndView.addObject(
+                ViewModelNames.ERROR_MESSAGE.getValue(),
+                e.getMessage());
+            modelAndView.addObject(
+                ViewModelNames.PRODUCTS.getValue(),
+                (new Product[0]));
+        }
+    }
+
+    @Autowired
+    private ProductsQuery productsQuery;
+}
