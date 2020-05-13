683cc00b96079f7b7ba68d8cfd283f6cc53b7f25
andrewburroughs <43488350+andrewburroughs@users.noreply.github.com>
2020-05-12 23:50:06 -0500

    Fixed Nonsense

--- src/main/java/edu/uark/registerapp/controllers/SalesReportRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SalesReportRouteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import edu.uark.registerapp.commands.products.ProductsQuery;
+import edu.uark.registerapp.commands.products.SortedProductsQuery;
 import edu.uark.registerapp.controllers.enums.ViewModelNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
 import edu.uark.registerapp.models.api.Product;
